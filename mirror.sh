e38d720292f02c201d02589aa9012a19a2dbfb6d
flavienbwk <flavien@berwick.fr>
2021-03-25 20:06:36 +0100

    Added base

---
+++ mirror.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -ex
+
+version="el7"
+src=http://mirrors.coreix.net/elrepo
+dest=/downloads
+
+for folder in "elrepo" "kernel"
+do
+	arch_src="$src/$folder/${version}"
+	arch_dest="$dest/$folder/$version"
+	mkdir -p "$arch_dest"
+	wget \
+		--mirror -nH --cut-dirs=1 --convert-links --adjust-extension --page-requisites --no-parent \
+		"$arch_src" -P "$arch_dest"
+done
+
